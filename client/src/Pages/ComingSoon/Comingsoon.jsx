630a6d06887b75397f29b7783479ca586e7ab81e
Piyush1638 <21bcs1638@cuchd.in>
2024-06-20 00:41:03 +0530

    sidebar and Topbar Created

--- client/src/Pages/ComingSoon/Comingsoon.jsx
+++ client/src/Pages/ComingSoon/Comingsoon.jsx
@@ -1,23 +1,21 @@
 // import React from 'react';
-import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
+import React from 'react';
+import Topbar from '../../components/topbar/Topbar';
+import Sidebar from '../../components/sidebar/Sidebar';
 import './Comingsoon.scss';
 
 const Comingsoon = () => {
     return (
-        <div className="comingsoon">
-            <div className="content">
-                <h1>We are Coming Soon</h1>
-                <p>Join our waitlist and be the first to know when the exclusive PHICSIT
-                    <br /> e-quiz Dashboard launches.</p>
-                <button className="navbar-button">Notify Me</button>
-                <div className="social-icons">
-                    <a href='https://www.facebook.com/PHICSIT' target="_blank" rel="noopener noreferrer" className="social-icon"><FaFacebookF /></a>
-                    <a href='https://x.com/phicsit' target="_blank" rel="noopener noreferrer" className="social-icon"><FaTwitter /></a>
-                    <a href="https://www.instagram.com/phicsit.in" target="_blank" rel="noopener noreferrer" className="social-icon"><FaInstagram /></a>
-                    <a href='https://www.linkedin.com/company/phicsit' target="_blank" rel="noopener noreferrer" className="social-icon"><FaLinkedinIn /></a>
+        <main className="app">
+            <Topbar />
+            <div className="main">
+                <Sidebar />
+                <div className="content">
+                    {/* Add your main content here */}
+                     <h1>Leaderboard</h1>
                 </div>
             </div>
-        </div>
+        </main>
     );
 }
 
