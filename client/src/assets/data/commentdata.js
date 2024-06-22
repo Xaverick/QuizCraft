c0f6b594951a64dd750201bdb9c7966783281c2c
Yash Mittal <mittalyas1234@gmail.com>
2024-06-22 21:39:14 +0530

    done css changes

--- client/src/assets/data/commentdata.js
+++ client/src/assets/data/commentdata.js
@@ -1,64 +1,66 @@
 import dumy3 from '../Contestimages/dumy3.png';
+import boy from '../Contestimages/Boy.png'
+import girl from '../Contestimages/Girl.png'
 const commentdata = [
     {
         id: 1,
-        name: 'John Doe',
+        name: 'Anushree Patel',
         comment: "I love the question quality of this contest platform. Competing across different topics is both challenging and fun. The global leaderboard after the contest keeps the competition intense.",
-        image: dumy3,
+        image: girl,
     },
     {
         id: 2,
-        name: 'John Doe',
+        name: 'Daksh Pandey',
         comment: "The contests are well-organized and cover a broad range of topics, making it a great platform for continuous learning. The global leaderboard keeps me motivated.",
-        image: dumy3,
+        image: boy,
     },
     {
         id: 3,
-        name: 'John Doe',
+        name: 'Radhika Jain',
         comment: "This platform is excellent for anyone looking to test and expand their knowledge. The variety of topics is impressive, and the leaderboard after every contest keeps the excitement high.",
-        image: dumy3,
+        image: girl,
     },
     {
         id: 4,
-        name: 'John Doe',
+        name: 'Pratik Kumar',
         comment: "This platform is excellent for anyone looking to test and expand their knowledge. The variety of topics is impressive, and the leaderboard after every contest keeps the excitement high.",
-        image: dumy3,
+        image: boy,
     },
     {
         id: 5,
-        name: 'John Doe',
+        name: 'Ananya Sharma',
         comment: "The contests here are both challenging and rewarding. The global leaderboard keeps me on my toes, and the opportunity to meet mentors has given me invaluable advice and support.",
-        image: dumy3,
+        image: girl,
     },
     {
         id: 6,
-        name: 'John Doe',
+        name: 'Mustafa Azad',
         comment: "This contest platform is very good! The diversity of topics ensures there's always something new to tackle, and the global leaderboard after contest keeps me engaged.",
-        image: dumy3,
+        image: boy,
     },
     {
         id: 7,
-        name: 'John Doe',
+        name: 'Payal Roy',
         comment: "The contests here are both challenging and rewarding. The global leaderboard keeps me on my toes, and the chance to win exciting rewards is a huge bonus.",
-        image: dumy3,
+        image: girl,
     },
     {
         id: 8,
-        name: 'John Doe',
+        name: 'Saurav Jain',
         comment: "Competing in these contests has been a fantastic experience. The diverse topics challenge me in new ways. The opportunity to win exciting rewards is the icing on the cake!",
-        image: dumy3,
+        image: boy,
     },
     {
         id: 9,
-        name: 'John Doe',
+        name: 'Ipsita Singh',
         comment: "This contest platform offers a perfect blend of learning and fun. The wide range of topics keeps me engaged, and the chance to win exciting rewards makes it all the more worthwhile!",
-        image: dumy3,
+        image: girl,
     },
     {
         id: 10,
-        name: 'John Doe',
+        name: 'Avnish Ahuja',
         comment: "The contests are well-organized and cover a broad range of topics, making it a great platform for continuous learning. The chance to win exciting rewards is a fantastic added benefit.",
-        image: dumy3,
+        image: boy,
     }
 ]
 export default commentdata;
